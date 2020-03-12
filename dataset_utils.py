c769635a7a4cb40cfd4df8bbf5f7fbe0d5b11ace
AlonLahav <alon.lahav2@gmai.com>
2020-03-12 19:30:59 +0200

    .

--- dataset_utils.py
+++ dataset_utils.py
@@ -8,6 +8,9 @@ import params
 # Get the data
 # ------------
 root_path_ds = '/media/alonlahav/4T-b/datasets/iwildcam-2019-fgvc6'
+train_csv_file = root_path_ds + '/train.csv'
+test_csv_file = root_path_ds + '/test.csv'
+
 
 def process_path_no_augmentation(label, file_name, location, n_frames):
   fn = root_path_ds + '/train_images/' + file_name
@@ -23,44 +26,41 @@ def process_path_no_augmentation(label, file_name, location, n_frames):
 def process_path(label, file_name, location, n_frames):
   if params.EXPLORE_DATASET and not params.SHOW_IMAGES:
     image = np.zeros(params.IMAGE_SHAPE)
+    label = tf.one_hot(label, params.num_classes)
   else:
-    fn = root_path_ds + '/train_images/' + file_name
-    image = tf.io.read_file(fn)
-    image = tf.image.decode_jpeg(image)
-    image = tf.image.convert_image_dtype(image, tf.float32)
-    if params.IMAGE_SHAPE[0] != -1 and not params.EXPLORE_DATASET:
-      image = tf.image.resize(image, (params.IMAGE_SHAPE[0] * params.SPLIT[0], params.IMAGE_SHAPE[0] * params.SPLIT[1]))
-  label = tf.one_hot(label, params.num_classes)
+    image, label = process_path_no_augmentation(label, file_name, location, n_frames)
 
-  if 0:
-    image = tf.cond(tf.math.equal(n_frames, 1), lambda: tf.image.grayscale_to_rgb(image),
-                    lambda: tf.identity(image))
-  if 0:
-    image = tf.cond(tf.math.equal(n_frames, 1), lambda: tf.concat((image, image, image), axis=2),
-                    lambda: tf.identity(image))
-
-  # data augmentation
-  image = tf.image.random_flip_left_right(image)
-  image = tf.image.random_hue(image, 0.5)
-  image = tf.image.random_brightness(image, 0.5)
-  image = tf.image.random_contrast(image, 0.25, 0.75)
+    # data augmentation
+    image = tf.image.random_flip_left_right(image)
+    image = tf.image.random_hue(image, 0.5)
+    image = tf.image.random_brightness(image, 0.5)
+    image = tf.image.random_contrast(image, 0.25, 0.75)
 
   return image, label # , location
 
-def get_dataset(locations='all', train=True):
+def get_dataset_kaggle_test():
+  pass
+
+def get_dataset(dataset='train/val', locations='all', train=True):
   def _filter_classes(image, label, location, n_frames):
     if label == 1:
       return True
     else:
       return False
 
-  train_csv_file = root_path_ds + '/train.csv'
   record_defaults = [-1, '--', -1, -1]
-  dataset = tf.data.experimental.CsvDataset(train_csv_file, record_defaults, select_cols=[0, 2, 5, 8], header=True) # category-id / file name / location
-  dataset = dataset.filter(lambda label, image, location, n_frames: tf.reduce_any(location == locations))
+
+  if dataset == 'kaggle-test':
+    csv_file = test_csv_file
+    select_cols = [2, 1, 4, 7]
+  else:
+    csv_file = train_csv_file
+    select_cols = [0, 2, 5, 8]
+  dataset = tf.data.experimental.CsvDataset(csv_file, record_defaults, select_cols=select_cols, header=True) # category-id / file name / location
+  if locations != 'all':
+    dataset = dataset.filter(lambda label, image, location, n_frames: tf.reduce_any(location == locations))
   if not params.EXPLORE_DATASET:
     dataset = dataset.filter(lambda label, image, location, n_frames: (n_frames == 3))
-
   if params.SMALL_DATASET:
     dataset = dataset.filter(lambda label, image, location, n_frames: (label == 1 or label == 17 or label == 18))
   dataset = dataset.shuffle(buffer_size=1000)
--- evaluate.py
+++ evaluate.py
@@ -1,13 +1,17 @@
+import csv
 
 import tensorflow as tf
+import numpy as np
 
 import dataset_utils
 import dnn_models
 import params
 
-dnn_models.config_gpu(0)
+dnn_models.config_gpu(1)
 
-run = 'logdir_11.03.2020..07.34'
+params.BATCH_SIZE = 1
+
+run = 'logdir_12.03.2020..17.17'
 model_dir = '/home/alonlahav/git-projects/dl_course_iWildCam/' + run
 
 model = dnn_models.get_model()
@@ -15,14 +19,30 @@ model.load_weights(model_dir + '/model/mymodel_1/variables/variables')
 
 if 0:   # Validate
   dataset = dataset_utils.get_dataset(locations=params.test_locations ,train=False)
+  csv_name = 'val'
 elif 1: # Train
   dataset = dataset_utils.get_dataset(locations=params.train_locations ,train=False)
+  csv_name = 'train'
 else:   # Test
   dataset = dataset_utils.get_dataset(locations='kaggle-test' ,train=False)
+  csv_name = 'test'
 
 
+results = []
 m = tf.keras.metrics.CategoricalAccuracy()
+i = 0
 for im, lb in dataset:
   pred = model.predict(im)
   _ = m.update_state(lb, pred)
   print(m.result().numpy())
+  category = np.argmax(pred)
+  id = '?'
+  results.append([i, id, category])
+  i += 1
+  #if i > 50:
+  #  break
+
+with open(model_dir + '/' + csv_name + '.csv', 'w', newline='') as fd:
+  for r in results:
+    fd.write(str(r[0]) + ' , ' + r[1] + ' , ' + str(r[2]) + '\n')
+
